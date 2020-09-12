f6f8b15f3ce68997eb7e2509d06e5ed7648bbf34
kanasaki15 <kassi_0123@kassi-hp.info>
2020-09-12 16:53:04 +0900

    データ部分再実装

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/FrameListener.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/FrameListener.java
@@ -1,5 +1,6 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,9 +11,20 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.UUID;
 
 
 class FrameListener implements Listener {
+
+    private final Plugin plugin;
+    private final Connection con;
+
+    public FrameListener(Plugin plugin, Connection con){
+        this.plugin = plugin;
+        this.con = con;
+    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void PlayerInteractEntityEvent (PlayerInteractEntityEvent e){
@@ -36,6 +48,60 @@ class FrameListener implements Listener {
     public void EntityDamageEvent (EntityDamageByEntityEvent e){
         // 額縁の中身を取り出されるとき
 
+    }
+
+
+    private FrameData getData(UUID itemFlame){
+        FrameData data = null;
+
+        try {
+            if (con != null){
+                if (!con.isClosed()){
+                    PreparedStatement statement = con.prepareStatement("SELECT * FROM IFPTable WHERE ItemFrame = ?");
+                    statement.setString(1, itemFlame.toString());
+                    ResultSet resultSet = statement.executeQuery();
+                    if (resultSet.next()){
+                        String createUser = resultSet.getString("CreateUser");
+                        String itemFrame = resultSet.getString("ItemFrame");
+
+                        return new FrameData(UUID.fromString(createUser), UUID.fromString(itemFrame));
+                    }
+                }
+            }
+        } catch (Exception e){
+            if (plugin.getConfig().getBoolean("errorPrint")){
+                plugin.getLogger().info(ChatColor.RED + "エラーを検知しました。");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        return data;
+    }
+
+    private void setData(UUID createUser, UUID itemFlame){
+        new Thread(() -> {
+            try {
+                if (con != null){
+                    if (getData(itemFlame) == null){
+                        PreparedStatement statement1 = con.prepareStatement("INSERT INTO `IFPTable` (`CreateUser`, `ItemFrame`) VALUES (?, ?);");
+                        statement1.setString(1, createUser.toString());
+                        statement1.setString(2, itemFlame.toString());
+                        statement1.execute();
+                    } else {
+                        PreparedStatement statement1 = con.prepareStatement("DELETE FROM `IFPTable` WHERE `CreateUser` = ? AND `ItemFrame` = ?");
+                        statement1.setString(1, createUser.toString());
+                        statement1.setString(2, itemFlame.toString());
+                        statement1.execute();
+                    }
+                }
+            } catch (Exception e) {
+                if (plugin.getConfig().getBoolean("errorPrint")){
+                    plugin.getLogger().info(ChatColor.RED + "エラーを検知しました。");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
@@ -1,22 +1,64 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.Executor;
+
 public final class ItemFrameProtectionPlugin extends JavaPlugin {
+
+    private Connection con = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        try {
+            saveDefaultConfig();
+
+            boolean useMySQL = false; // getConfig().getBoolean("useMySQL");
+
+            if (useMySQL){
+                final String MySQLServer = getConfig().getString("MySQLServer");
+                final String MySQLUsername = getConfig().getString("MySQLUsername");
+                final String MySQLPassword = getConfig().getString("MySQLPassword");
+                final String MySQLDatabase = getConfig().getString("MySQLDatabase");
+                final String MySQLOption = getConfig().getString("MySQLOption");
+                con = DriverManager.getConnection("jdbc:mysql://" + MySQLServer + "/" + MySQLDatabase + MySQLOption, MySQLUsername, MySQLPassword);
+            } else {
+                String pass = "./" + getDataFolder().getPath() + "/data.db";
+                con = DriverManager.getConnection("jdbc:sqlite:"+pass);
+                con.setAutoCommit(true);
+            }
+
+            getPluginLoader().createRegisteredListeners(new FrameListener(this, con),this);
+        } catch (Exception e){
+            if (getConfig().getBoolean("errorPrint")){
+                getLogger().info(ChatColor.RED + "エラーを検知しました。");
+                e.printStackTrace();
+            }
+            onDisable();
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    @Override
-    public void onLoad() {
-        // super.onLoad();
-
+        if (con != null){
+            new Thread(
+                    () -> {
+                        try {
+                            con.close();
+                        } catch (Exception e){
+                            if (getConfig().getBoolean("errorPrint")){
+                                getLogger().info(ChatColor.RED + "エラーを検知しました。");
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            );
+        }
     }
 }
