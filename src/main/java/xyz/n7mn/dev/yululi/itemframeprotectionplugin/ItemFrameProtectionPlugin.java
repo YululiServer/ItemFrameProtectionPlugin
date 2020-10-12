9b492578fc505438cfae5fe0eb72660f4a9f5db8
kanasaki15 <kassi_0123@kassi-hp.info>
2020-10-13 01:25:07 +0900

    終了時エラー修正

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
@@ -210,111 +210,108 @@ public final class ItemFrameProtectionPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (con != null) {
+        new Thread(()->{
+            if (con != null) {
+                try {
+                    bukkitTask.cancel();
+                    bukkitTask2.cancel();
+
                     try {
-                        bukkitTask.cancel();
-                        bukkitTask2.cancel();
-
-                        try {
-                            List<FrameData> itemFrameList = data.getItemFrameList();
-                            // System.out.println("Debug : LockFrame : " + itemFrameList.size());
-                            synchronized (itemFrameList) {
-                                for (FrameData data : itemFrameList) {
-                                    PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) FROM IFPTable WHERE ItemFrame = ?");
-                                    statement.setString(1, data.getItemFrame().toString());
-                                    ResultSet set = statement.executeQuery();
-                                    if (set.next()) {
-                                        if (set.getInt("COUNT(*)") == 0) {
-                                            statement.close();
-                                            PreparedStatement statement1 = con.prepareStatement("INSERT INTO `IFPTable` (`CreateUser`, `ItemFrame`) VALUES (?, ?);");
-                                            statement1.setString(1, data.getCreateUser().toString());
-                                            statement1.setString(2, data.getItemFrame().toString());
-                                            statement1.execute();
-                                            statement1.close();
-                                        }
-                                    }
-                                }
-                            }
-                            // System.out.println("Debug : LockFrame : " + itemFrameList.size());
-
-
-                            List<DropData> dropList = data.getDropList();
-                            // System.out.println("Debug : dropItem : " + dropList.size());
-                            synchronized (dropList) {
-                                for (DropData data : dropList) {
-                                    PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) FROM IFPTable2 WHERE ItemUUID = ?");
-                                    statement.setString(1, data.getItemUUID().toString());
-                                    ResultSet set = statement.executeQuery();
-                                    if (set.next()) {
-                                        if (set.getInt("COUNT(*)") == 0) {
-                                            statement.close();
-                                            PreparedStatement statement1 = con.prepareStatement("INSERT INTO `IFPTable2` (`DropUser`, `ItemUUID`) VALUES (?, ?);");
-                                            statement1.setString(1, data.getDropUser().toString());
-                                            statement1.setString(2, data.getItemUUID().toString());
-                                            statement1.execute();
-                                            statement1.close();
-                                        }
-                                    }
-                                }
-                            }
-
-
-                            // ここからゴミデータお掃除
-
-                            List<World> worlds = getServer().getWorlds();
-                            int i = 0;
-                            for (World world : worlds) {
-                                i = i + world.getEntities().size();
-                            }
-                            if (i != 0) {
-                                PreparedStatement statement = con.prepareStatement("SELECT * FROM IFPTable");
+                        List<FrameData> itemFrameList = data.getItemFrameList();
+                        // System.out.println("Debug : LockFrame : " + itemFrameList.size());
+                        synchronized (itemFrameList) {
+                            for (FrameData data : itemFrameList) {
+                                PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) FROM IFPTable WHERE ItemFrame = ?");
+                                statement.setString(1, data.getItemFrame().toString());
                                 ResultSet set = statement.executeQuery();
-                                while (set.next()) {
-                                    FrameData itemFrame = data.getItemFrame(UUID.fromString(set.getString("CreateUser")), UUID.fromString(set.getString("ItemFrame")));
-                                    if (itemFrame == null) {
-                                        PreparedStatement statement1 = con.prepareStatement("DELETE FROM `IFPTable` WHERE `CreateUser` = ? AND `ItemFrame` = ?");
-                                        statement1.setString(1, set.getString("CreateUser"));
-                                        statement1.setString(2, set.getString("ItemFrame"));
+                                if (set.next()) {
+                                    if (set.getInt("COUNT(*)") == 0) {
+                                        statement.close();
+                                        PreparedStatement statement1 = con.prepareStatement("INSERT INTO `IFPTable` (`CreateUser`, `ItemFrame`) VALUES (?, ?);");
+                                        statement1.setString(1, data.getCreateUser().toString());
+                                        statement1.setString(2, data.getItemFrame().toString());
                                         statement1.execute();
                                         statement1.close();
                                     }
                                 }
-                                statement.close();
-
-                                PreparedStatement statement2 = con.prepareStatement("SELECT * FROM IFPTable2");
-                                ResultSet set2 = statement2.executeQuery();
-                                while (set2.next()) {
-                                    FrameData itemFrame = data.getItemFrame(UUID.fromString(set2.getString("DropUser")), UUID.fromString(set2.getString("ItemUUID")));
-                                    if (itemFrame == null) {
-                                        PreparedStatement statement1 = con.prepareStatement("DELETE FROM `IFPTable2` WHERE `DropUser` = ? AND `ItemUUID` = ?");
-                                        statement1.setString(1, set2.getString("DropUser"));
-                                        statement1.setString(2, set2.getString("ItemUUID"));
-                                        statement1.execute();
-                                        statement1.close();
-                                    }
-                                }
-                                statement2.close();
-                            }
-
-                            con.close();
-                        } catch (Exception e) {
-                            if (getConfig().getBoolean("errorPrint")) {
-                                getLogger().info(ChatColor.RED + "エラーを検知しました。");
-                                e.printStackTrace();
                             }
                         }
+                        // System.out.println("Debug : LockFrame : " + itemFrameList.size());
+
+
+                        List<DropData> dropList = data.getDropList();
+                        // System.out.println("Debug : dropItem : " + dropList.size());
+                        synchronized (dropList) {
+                            for (DropData data : dropList) {
+                                PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) FROM IFPTable2 WHERE ItemUUID = ?");
+                                statement.setString(1, data.getItemUUID().toString());
+                                ResultSet set = statement.executeQuery();
+                                if (set.next()) {
+                                    if (set.getInt("COUNT(*)") == 0) {
+                                        statement.close();
+                                        PreparedStatement statement1 = con.prepareStatement("INSERT INTO `IFPTable2` (`DropUser`, `ItemUUID`) VALUES (?, ?);");
+                                        statement1.setString(1, data.getDropUser().toString());
+                                        statement1.setString(2, data.getItemUUID().toString());
+                                        statement1.execute();
+                                        statement1.close();
+                                    }
+                                }
+                            }
+                        }
+
+
+                        // ここからゴミデータお掃除
+
+                        List<World> worlds = getServer().getWorlds();
+                        int i = 0;
+                        for (World world : worlds) {
+                            i = i + world.getEntities().size();
+                        }
+                        if (i != 0) {
+                            PreparedStatement statement = con.prepareStatement("SELECT * FROM IFPTable");
+                            ResultSet set = statement.executeQuery();
+                            while (set.next()) {
+                                FrameData itemFrame = data.getItemFrame(UUID.fromString(set.getString("CreateUser")), UUID.fromString(set.getString("ItemFrame")));
+                                if (itemFrame == null) {
+                                    PreparedStatement statement1 = con.prepareStatement("DELETE FROM `IFPTable` WHERE `CreateUser` = ? AND `ItemFrame` = ?");
+                                    statement1.setString(1, set.getString("CreateUser"));
+                                    statement1.setString(2, set.getString("ItemFrame"));
+                                    statement1.execute();
+                                    statement1.close();
+                                }
+                            }
+                            statement.close();
+
+                            PreparedStatement statement2 = con.prepareStatement("SELECT * FROM IFPTable2");
+                            ResultSet set2 = statement2.executeQuery();
+                            while (set2.next()) {
+                                FrameData itemFrame = data.getItemFrame(UUID.fromString(set2.getString("DropUser")), UUID.fromString(set2.getString("ItemUUID")));
+                                if (itemFrame == null) {
+                                    PreparedStatement statement1 = con.prepareStatement("DELETE FROM `IFPTable2` WHERE `DropUser` = ? AND `ItemUUID` = ?");
+                                    statement1.setString(1, set2.getString("DropUser"));
+                                    statement1.setString(2, set2.getString("ItemUUID"));
+                                    statement1.execute();
+                                    statement1.close();
+                                }
+                            }
+                            statement2.close();
+                        }
+
+                        con.close();
                     } catch (Exception e) {
                         if (getConfig().getBoolean("errorPrint")) {
                             getLogger().info(ChatColor.RED + "エラーを検知しました。");
                             e.printStackTrace();
                         }
                     }
+                } catch (Exception e) {
+                    if (getConfig().getBoolean("errorPrint")) {
+                        getLogger().info(ChatColor.RED + "エラーを検知しました。");
+                        e.printStackTrace();
+                    }
                 }
             }
-        }.runTaskLaterAsynchronously(this, 0L);
+        }).start();
 
     }
 }
