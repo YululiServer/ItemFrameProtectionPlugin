0a2ee921e324d29c35bdee24d705a1df3f336ade
kanasaki15 <kassi_0123@kassi-hp.info>
2020-10-19 22:02:20 +0900

    コマンド作り込み

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/IFPCommand.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/IFPCommand.java
@@ -1,7 +1,10 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -26,6 +29,119 @@ class IFPCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player){
+
+            Player player = (Player) sender;
+
+            if (args.length == 0){
+
+                sender.sendMessage("額縁保護プラグイン Ver " + plugin.getDescription().getVersion());
+                if (player.hasPermission("ifp.op")){
+
+                    sender.sendMessage(ChatColor.GOLD + "/ifp admin --- 全部出す");
+                    sender.sendMessage(ChatColor.GOLD + "/ifp user <UserName> --- 指定したユーザーの額縁リストを出す");
+
+                }
+
+                TextComponent text = new TextComponent();
+                TextComponent click = new TextComponent(ChatColor.YELLOW + "すべてを表示する場合はこちらをクリック！");
+                click.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/ifp all"));
+                text.addExtra(click);
+                sender.sendMessage(text);
+
+                int i = 0;
+
+                List<FrameData> list = api.getListByFrameData(true);
+                for (FrameData data : list){
+
+                    Entity entity = Bukkit.getEntity(data.getItemFrameUUID());
+
+                    if (entity == null){
+                        continue;
+                    }
+
+                    if (!data.getProtectUser().equals(player.getUniqueId())){
+                        continue;
+                    }
+
+                    if (i > 5){
+                        break;
+                    }
+
+                    Location location = entity.getLocation();
+
+                    StringBuffer sb = new StringBuffer();
+
+                    sb.append("ワールド名 : ");
+                    sb.append(location.getWorld().getName());
+                    sb.append(" X: ");
+                    sb.append(location.getBlockX());
+                    sb.append(" Y: ");
+                    sb.append(location.getBlockY());
+                    sb.append(" Z: ");
+                    sb.append(location.getBlockZ());
+
+                    String dataText = sb.toString();
+
+                    if (player.hasPermission("ifp.op")){
+
+                        TextComponent text1 = new TextComponent(dataText);
+                        TextComponent click1 = new TextComponent(ChatColor.AQUA + "[Teleport]");
+                        click1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tp "+ location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ()));
+                        text.addExtra(click1);
+                        sender.sendMessage(text1);
+
+                    } else {
+                        sender.sendMessage(dataText);
+                    }
+
+                    i++;
+                }
+
+            }
+
+            if (args.length == 1){
+
+                if (args[0].toLowerCase().equals("admin")){
+                    List<FrameData> list = api.getListByFrameData(false);
+                    sender.sendMessage(ChatColor.GREEN + "保護件数 : " + list.size() + " 件");
+
+                    for (FrameData data : list){
+
+                        Entity entity = Bukkit.getEntity(data.getItemFrameUUID());
+                        if (entity == null){
+                            continue;
+                        }
+
+                        if (data.isActive()){
+                            sender.sendMessage(ChatColor.GREEN + "■ "+ChatColor.RESET+"UUID: " + data.getItemFrameUUID() + " WorldName: " + entity.getLocation().getWorld().getName() + " X: " + entity.getLocation().getBlockX() + " Y: " + entity.getLocation().getBlockY() + " Z: " + entity.getLocation().getBlockZ() + " UserUUID: " + data.getCreateDate());
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "■ "+ChatColor.RESET+"UUID: " + data.getItemFrameUUID() + " WorldName: " + entity.getLocation().getWorld().getName() + " X: " + entity.getLocation().getBlockX() + " Y: " + entity.getLocation().getBlockY() + " Z: " + entity.getLocation().getBlockZ() + " UserUUID: " + data.getCreateDate());
+                        }
+
+                    }
+                }
+
+                if (args[0].toLowerCase().equals("all")){
+                    sender.sendMessage(ChatColor.YELLOW + "準備中...");
+                }
+            }
+
+            if (args.length == 2){
+
+                if (args[0].toLowerCase().equals("user") && player.hasPermission("ifp.op")){
+
+                    Player targetPlayer = Bukkit.getPlayer(args[1]);
+                    if (targetPlayer == null){
+
+                        sender.sendMessage(ChatColor.RED + "プレーヤーが見つかりません。");
+                        return true;
+                    }
+
+                    sender.sendMessage("---- " + targetPlayer.getName() + "さんの額縁保護リスト ----");
+
+                }
+
+            }
 
 
 
