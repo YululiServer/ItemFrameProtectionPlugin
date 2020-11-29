22b5aaed730060e4b19067271312b51e0a3f8ebd
kanasaki15 <kassi_0123@kassi-hp.info>
2020-11-30 02:48:05 +0900

    Ver 4.x始動

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
@@ -1,27 +1,8 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.n7mn.dev.yululi.itemframeprotectionplugin.data.DataAPI;
-import xyz.n7mn.dev.yululi.itemframeprotectionplugin.data.DebugTimer;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.*;
 public final class ItemFrameProtectionPlugin extends JavaPlugin {
-
-
-
-    private Connection con = null;
-    private DataAPI dataAPI = null;
-
-    public DataAPI getDataAPI() {
-        return dataAPI;
-    }
-
-    void setConnect(Connection con ){
-        this.con = con;
-    }
 
     @Override
     public void onEnable() {
@@ -34,137 +15,12 @@ public final class ItemFrameProtectionPlugin extends JavaPlugin {
         String MySQLDatabase = getConfig().getString("MySQLDatabase");
         String MySQLOption = getConfig().getString("MySQLOption");
 
-        try {
-
-            if (getConfig().getBoolean("useMySQL")) {
-
-                con = DriverManager.getConnection("jdbc:mysql://" + MySQLServer + "/" + MySQLDatabase + MySQLOption, MySQLUsername, MySQLPassword);
-                con.setAutoCommit(true);
-                dataAPI = new DataAPI(con, this);
-
-                try {
-                    PreparedStatement statement = con.prepareStatement("SELECT 1 FROM ItemFrameTable1 LIMIT 1;");
-                    statement.execute();
-                    statement.close();
-                } catch (SQLException e){
-                    dataAPI.createTableByItem();
-                }
-
-                try {
-                    PreparedStatement statement = con.prepareStatement("SELECT 1 FROM ItemFrameTable2 LIMIT 1;");
-                    statement.execute();
-                    statement.close();
-                } catch (SQLException e){
-                    dataAPI.createTableByDrop();
-                }
-
-                try {
-                    PreparedStatement statement = con.prepareStatement("SELECT 1 FROM ItemFrameTable3 LIMIT 1;");
-                    statement.execute();
-                    statement.close();
-                } catch (SQLException e){
-                    dataAPI.createTableByBox();
-                }
-
-            } else {
-
-                String pass = "./" + getDataFolder().getPath() + "/FrameData.db";
-                if (System.getProperty("os.name").toLowerCase().startsWith("windows")){
-                    pass = pass.replaceAll("/", "\\\\");
-                }
-
-                boolean cre = false;
-                if (!new File(pass).exists()){
-                    try {
-                        cre = new File(pass).createNewFile();
-                    } catch (IOException e) {
-                        // e.printStackTrace();
-                    }
-                }
-                con = DriverManager.getConnection("jdbc:sqlite:"+pass);
-                con.setAutoCommit(true);
-
-                dataAPI = new DataAPI(con, this);
-                if (cre) {
-                    dataAPI.createAllTable();
-                } else {
-
-                    try {
-                        PreparedStatement statement = con.prepareStatement("SELECT 1 FROM ItemFrameTable1 LIMIT 1;");
-                        statement.execute();
-                        statement.close();
-                    } catch (SQLException e){
-                        dataAPI.createTableByItem();
-                    }
-
-                    try {
-                        PreparedStatement statement = con.prepareStatement("SELECT 1 FROM ItemFrameTable2 LIMIT 1;");
-                        statement.execute();
-                        statement.close();
-                    } catch (SQLException e){
-                        dataAPI.createTableByDrop();
-                    }
-
-                    try {
-                        PreparedStatement statement = con.prepareStatement("SELECT 1 FROM ItemFrameTable3 LIMIT 1;");
-                        statement.execute();
-                        statement.close();
-                    } catch (SQLException e){
-                        dataAPI.createTableByBox();
-                    }
-
-                }
-
-            }
-
-            getCommand("ifp").setExecutor(new IFPCommand(dataAPI));
-            getCommand("ifp").setTabCompleter(new IFPCommandTab());
-            getCommand("frame-kill").setExecutor(new IFPCommand(dataAPI));
-
-            getServer().getPluginManager().registerEvents(new ItemFrameListener(dataAPI), this);
-
-            new AutoRemoveTimer(dataAPI, this).runTaskLaterAsynchronously(this, 20L);
-            new AutoSQLConnectCheckTimer(this, con).runTaskLaterAsynchronously(this, 20L);
-
-            if (this.getDescription().getVersion().endsWith("-dev")){
-                new DebugTimer(dataAPI, this).runTaskLaterAsynchronously(this, 0L);
-            }
-
-            getLogger().info("Started ItemFrameProtectionPlugin Ver "+getDescription().getVersion()+"!!");
-
-        } catch (Exception e){
-            if (getConfig().getBoolean("errorPrint")){
-                getLogger().info(ChatColor.RED + "エラーを検知しました。");
-                e.printStackTrace();
-            }
-            onDisable();
-        }
+        getLogger().info("Started ItemFrameProtectionPlugin Ver "+getDescription().getVersion()+"!!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-
-        new Thread(()->{
-
-            try {
-                if (con != null){
-
-                    dataAPI.cacheToSQL();
-                    con.close();
-
-                }
-            } catch (Exception e){
-
-                if (getConfig().getBoolean("errorPrint")){
-                    getLogger().info(ChatColor.RED + "エラーを検知しました。");
-                    e.printStackTrace();
-                }
-
-            }
-
-        }).start();
-
         getLogger().info("Disabled ItemFrameProtectionPlugin Ver "+getDescription().getVersion()+"!!");
     }
 }
