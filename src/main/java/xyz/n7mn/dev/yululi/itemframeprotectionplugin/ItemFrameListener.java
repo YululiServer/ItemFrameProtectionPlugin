7e12602675f1355a7f55c8b8da4f2ebf9b10d521
kanasaki15 <kassi_0123@kassi-hp.info>
2020-12-06 14:36:19 +0900

    clear対策

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameListener.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameListener.java
@@ -14,10 +14,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.*;
 import org.bukkit.event.hanging.HangingBreakEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.*;
-import org.bukkit.material.FlowerPot;
 import org.bukkit.plugin.Plugin;
 import xyz.acrylicstyle.achievement.gui.ExtraInventoryGui;
 import xyz.acrylicstyle.achievement.gui.ExtraInventoryGuiHolder;
@@ -30,11 +30,37 @@ class ItemFrameListener implements Listener {
     final private DataAPI api;
     final private Plugin plugin = Bukkit.getPluginManager().getPlugin("ItemFrameProtectionPlugin");
 
+    private List<InventoryData> inventoryData = Collections.synchronizedList(new ArrayList<>());
+
     private Set<UUID> frameBreakList = Collections.synchronizedSet(new HashSet<>());
 
     public ItemFrameListener(DataAPI api){
 
         this.api = api;
+
+    }
+
+    @EventHandler
+    public void PlayerCommandPreprocessEvent (PlayerCommandPreprocessEvent e){
+
+        if (e.getMessage().equals("/clear") || e.getMessage().equals("/clear @p") || e.getMessage().equals("/clear @s") || e.getMessage().equals("/clear @a") || e.getMessage().equals("/clear " + e.getPlayer().getName())){
+
+            boolean c = false;
+            synchronized (inventoryData){
+                for (InventoryData data : inventoryData){
+
+                    if (data.getUuid().equals(e.getPlayer().getUniqueId())){
+                        data.setPlayerInventory(e.getPlayer().getInventory());
+                        c = true;
+                    }
+
+                }
+
+                if (!c){
+                    inventoryData.add(new InventoryData(e.getPlayer().getUniqueId(), e.getPlayer().getInventory()));
+                }
+            }
+        }
 
     }
 
@@ -339,6 +365,35 @@ class ItemFrameListener implements Listener {
                     for (int i = 0; i < size; i++){
 
                         ItemStack item = dataInventory.getItem(i);
+
+                        if (item != null && item.getType() == frame.getItem().getType() && ItemStackEqual(item, frame.getItem())){
+
+                            frame.setItem(new ItemStack(Material.AIR));
+                            e.setCancelled(true);
+                            return;
+                        }
+
+                    }
+
+                }
+
+                // clearされた直後だったら？
+
+                PlayerInventory playerInventory = null;
+                synchronized (inventoryData){
+                    for (InventoryData data : inventoryData){
+                        if (data.getUuid().equals(player.getUniqueId())){
+                            playerInventory = data.getPlayerInventory();
+                            break;
+                        }
+                    }
+                }
+
+                if (playerInventory != null){
+
+                    for (int i = 0; i < playerInventory.getSize(); i++){
+
+                        ItemStack item = playerInventory.getItem(i);
 
                         if (item != null && item.getType() == frame.getItem().getType() && ItemStackEqual(item, frame.getItem())){
 
