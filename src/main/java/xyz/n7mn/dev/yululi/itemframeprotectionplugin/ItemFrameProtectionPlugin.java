4aeca9b61ac85cc9708d62a6d57e585fc65bae9a
kanasaki15 <kassi_0123@kassi-hp.info>
2021-02-26 20:02:41 +0900

    中途半端過ぎて忘れたのでいちから書き直す

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
@@ -1,32 +1,7 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
-import org.bukkit.*;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.PistonMoveReaction;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Pose;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.metadata.MetadataValue;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.BoundingBox;
-import org.bukkit.util.Vector;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import xyz.acrylicstyle.paper.nbt.NBTTagCompound;
 
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemFrameProtectionPlugin extends JavaPlugin {
 
