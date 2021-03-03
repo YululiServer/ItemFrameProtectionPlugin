ab57496f5fc2bc41b440855112849162b6c55ccd
kanasaki15 <kassi_0123@kassi-hp.info>
2021-03-03 20:53:31 +0900

    SQLite対応やめました。あと設定ファイルとか変えたり

--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ItemFrameProtectionPlugin.java
@@ -1,9 +1,15 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
-
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public final class ItemFrameProtectionPlugin extends JavaPlugin {
+
+    private Connection con;
 
     @Override
     public void onEnable() {
@@ -11,10 +17,33 @@ public final class ItemFrameProtectionPlugin extends JavaPlugin {
         saveDefaultConfig();
 
         String MySQLServer = getConfig().getString("MySQLServer");
+        int MySQLPort = 3306;
+        if (getConfig().getInt("MySQLPort") != 3306 && getConfig().getInt("MySQLPort") <= 65535){
+            MySQLPort = getConfig().getInt("MySQLPort");
+        }
         String MySQLUsername = getConfig().getString("MySQLUsername");
         String MySQLPassword = getConfig().getString("MySQLPassword");
         String MySQLDatabase = getConfig().getString("MySQLDatabase");
         String MySQLOption = getConfig().getString("MySQLOption");
+
+        try {
+
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+            Connection connection = DriverManager.getConnection("" +
+                            "jdbc:mysql://" + MySQLServer + ":" + MySQLPort + "/" + MySQLDatabase + MySQLOption,
+                    MySQLUsername,
+                    MySQLPassword
+            );
+
+            con = connection;
+            con.setAutoCommit(true);
+        } catch (Exception e){
+            e.printStackTrace();
+            Bukkit.getServer().getPluginManager().disablePlugin(this);
+        }
+
+        getServer().getPluginManager().registerEvents(new ProtectListener(con, this), this);
 
         getLogger().info("Started ItemFrameProtectionPlugin Ver "+getDescription().getVersion()+"!!");
     }
@@ -22,6 +51,24 @@ public final class ItemFrameProtectionPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        if (con != null){
+
+            try {
+                new Thread(() -> {
+                    try {
+                        con.close();
+                    } catch (SQLException throwable) {
+                        //throwable.printStackTrace();
+                    }
+                });
+            } catch (Exception e){
+                // e.printStackTrace();
+            }
+
+        }
+
+
         getLogger().info("Disabled ItemFrameProtectionPlugin Ver "+getDescription().getVersion()+"!!");
     }
 }
--- src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ProtectListener.java
+++ src/main/java/xyz/n7mn/dev/yululi/itemframeprotectionplugin/ProtectListener.java
@@ -1,6 +1,20 @@
 package xyz.n7mn.dev.yululi.itemframeprotectionplugin;
 
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+import java.sql.Connection;
 
 class ProtectListener implements Listener {
+
+    private Connection con;
+    private final Plugin plugin;
+
+    public ProtectListener(Connection con, Plugin plugin){
+
+        this.con = con;
+        this.plugin = plugin;
+
+    }
+
 }
